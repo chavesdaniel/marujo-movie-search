67f73500a44554b7ccb9ff67a90aa98a84e13316
chavesdaniel <chavesdaniel@gmail.com>
2022-03-01 13:13:07 -0300

    estrutura sem storyboard

---
+++ MarujoMovieSearcher/MarujoMovieSearcher/MovieListVC.swift
@@ -0,0 +1,17 @@
+//
+//  MovieListVC.swift
+//  MarujoMovieSearcher
+//
+//  Created by Daniel on 01/03/22.
+//
+
+import UIKit
+
+class MovieListVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemPink
+    }
+
+}
